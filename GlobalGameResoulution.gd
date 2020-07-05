79de49bcbfad416931ab4d9406d5b9aaae4a3613
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-07-04 21:17:15 -1000

    add comments

--- GlobalGameResoulution.gd
+++ GlobalGameResoulution.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name Resolution
 
+# Static Functions for our default game development window
+# This is set to project settings
+# if you change project settings, you need to change this as well
 static func gameDevWidth() -> float:
 	return 1024.0
 
