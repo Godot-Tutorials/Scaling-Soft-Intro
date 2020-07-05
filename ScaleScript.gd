fb8b605c81c3a66095a5ada104fec35f43c275d0
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-07-04 21:20:42 -1000

    add comment

--- ScaleScript.gd
+++ ScaleScript.gd
@@ -9,6 +9,7 @@ func _enter_tree():
 	# an easier way to do this is to in fact have the scaling value set in a Global Class/Singleton where we can pull the scale fraction/float value
 	var scaleWidth = deviceManager.newDeviceWidth / Resolution.gameDevWidth()
 
+	# grab new screen size and center our image
 	var windowSize = OS.get_window_size()
 	# print(windowSize)
 	
